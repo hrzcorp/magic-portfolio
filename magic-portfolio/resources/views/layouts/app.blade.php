d42c05d478c733ca50a4d09d8e3dcdc9f06d8402
Latifah <112627375+Tifarin@users.noreply.github.com>
2024-08-18 19:15:07 +0700

    [MP - 36] - Carousel Testimonial (#12)
    * [WP - 12] - Our Goals
    
    * [WP - 16] - How to
    
    * [MP - 36] - Carousel Testimonial

--- magic-portfolio/resources/views/layouts/app.blade.php
+++ magic-portfolio/resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <title>{{ env('APP_NAME') }} - {{ $title ?? '' }}</title>
     @vite('resources/css/app.css')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css">
 </head>
 <body>
     {{ $slot }}
