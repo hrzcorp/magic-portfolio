43d6c4839d3ca65d735e45b437afc3b9bd7f046e
Tifarin <latifahlatifah443@gmail.com>
2024-08-18 19:09:56 +0700

    [MP - 35] - Contact Us

--- magic-portfolio/resources/views/home.blade.php
+++ magic-portfolio/resources/views/home.blade.php
@@ -71,7 +71,7 @@
         </div>
     </section>
 
-    <section class="bg-[#F8FAFC] px-6 pt-[90px] pb-[78px] rounded-[120px]">
+    <section class="bg-[#F8FAFC] relative z-[999] px-6 pt-[90px] pb-[78px] rounded-3xl md:rounded-[120px]">
         <div class="container relative">
             <h1 class="font-satoshi font-bold text-center max-w-2xl mx-auto text-3xl tracking-tight xl:leading-[4.5rem] lg:text-6xl">What our client think about us?</h1>
             <div class="slider-testimonial my-16">
@@ -111,6 +111,83 @@
                     </defs>
                 </svg>
             </div>
+        </div>
+    </section>
+
+    <section class="relative -mt-32 bg-[#373BD7] text-white px-6 rounded-b-3xl lg:rounded-b-[120px]">
+        <div class="container flex pt-56 pb-32 flex-col lg:flex-row justify-between items-center">
+          <div class="mb-8 max-w-[482px]">
+            <h2 class="text-xl font-medium">Contact us</h2>
+            <h3 class="text-2xl md:text-5xl font-satoshi font-bold xl:leading-[4rem] pt-6">Any questions? Write or call us.<br> We will write back within 12h</h3>
+            <p class="my-7 text-sm md:text-base font-medium">
+              Lorem ipsum dolor sit amet consectetur. Massa enim auctor aliquet proin convallis sed consectetur. Purus diam ultrices consectetur volutpat ut commodo leo ultrices velit.
+            </p>
+            <div class="hp:flex items-center justify-between">
+              <p class="flex items-center gap-[14px] text-sm md:text-base">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20.2638 16.7644L16.2371 13.1032C16.0467 12.9302 15.7966 12.8379 15.5395 12.8459C15.2824 12.8538 15.0384 12.9614 14.8591 13.1458L12.4886 15.5836C11.9181 15.4747 10.7709 15.1171 9.59015 13.9393C8.40936 12.7575 8.05175 11.6074 7.94576 11.0408L10.3816 8.66932C10.5663 8.49015 10.674 8.24616 10.682 7.98899C10.6899 7.73182 10.5975 7.48164 10.4242 7.29141L6.76398 3.26565C6.59066 3.07482 6.34979 2.95907 6.0925 2.94297C5.83522 2.92688 5.58179 3.01172 5.38605 3.17947L3.23646 5.02296C3.06519 5.19484 2.96297 5.4236 2.94918 5.66585C2.93432 5.9135 2.65101 11.7798 7.19984 16.3305C11.1682 20.2979 16.139 20.5881 17.508 20.5881C17.7081 20.5881 17.8309 20.5822 17.8636 20.5802C18.1058 20.5666 18.3345 20.4639 18.5055 20.2919L20.3481 18.1413C20.5165 17.9462 20.6019 17.693 20.5862 17.4357C20.5705 17.1785 20.4548 16.9376 20.2638 16.7644Z" fill="white"/>
+                </svg>                    
+                +1 342 332 338
+              </p>
+              <p class="flex items-center gap-[14px] text-sm md:text-base">
+                <svg width="20" height="17" viewBox="0 0 20 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16.6031 0.411743H3.2208C2.46025 0.411743 1.73085 0.7313 1.19306 1.30011C0.655276 1.86893 0.353149 2.64041 0.353149 3.44483V13.5551C0.353149 14.3596 0.655276 15.131 1.19306 15.6998C1.73085 16.2687 2.46025 16.5882 3.2208 16.5882H16.6031C17.3637 16.5882 18.0931 16.2687 18.6309 15.6998C19.1687 15.131 19.4708 14.3596 19.4708 13.5551V3.44483C19.4708 2.64041 19.1687 1.86893 18.6309 1.30011C18.0931 0.7313 17.3637 0.411743 16.6031 0.411743ZM15.9627 2.4338L9.91197 7.23619L3.86124 2.4338H15.9627ZM16.6031 14.5662H3.2208C2.96728 14.5662 2.72415 14.4596 2.54489 14.27C2.36562 14.0804 2.26491 13.8233 2.26491 13.5551V3.69759L9.33844 9.3088C9.5039 9.44006 9.70515 9.51101 9.91197 9.51101C10.1188 9.51101 10.32 9.44006 10.4855 9.3088L17.559 3.69759V13.5551C17.559 13.8233 17.4583 14.0804 17.2791 14.27C17.0998 14.4596 16.8567 14.5662 16.6031 14.5662Z" fill="white"/>
+                </svg>                    
+                info@allhire.com
+              </p>
+            </div>
+          </div>
+      
+          <div class="md:w-[428px] w-full text-black">
+            <form>
+              <div class="mb-5">
+                <input id="name" type="text" placeholder="Your Name" class="w-full p-2 md:p-5 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-600" />
+              </div>
+              <div class="mb-5">
+                <input id="email" type="email" placeholder="me@user.com" class="w-full p-2 md:p-5 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-600" />
+              </div>
+              <div class="mb-5">
+                <select id="title" placeholder="your title" class="w-full p-2 md:p-5 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-600">
+                    <option disabled selected>Your Title</option>
+                    <option>Mr</option>
+                    <option>Mrs</option>
+                    <option>Ms</option>
+                    <option>Dr</option>
+                </select>
+              </div>
+              <div class="mb-5">
+                <textarea id="message" placeholder="Message" class="w-full p-2 md:p-5 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-600 h-24"></textarea>
+              </div>
+              <button type="submit" class="w-full flex items-center gap-4 justify-center bg-[#3FB7BC] text-white font-bold py-2 md:py-4 px-4 md:px-6 rounded-xl hover:from-teal-500 hover:to-blue-700 transition-all duration-300">
+                Send
+                <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.3 11.275C9.1 11.075 9.004 10.8334 9.012 10.55C9.02 10.2667 9.12434 10.025 9.325 9.82505L12.15 7.00005H1C0.71667 7.00005 0.479004 6.90405 0.287004 6.71205C0.0950036 6.52005 -0.000663206 6.28272 3.4602e-06 6.00005C3.4602e-06 5.71672 0.0960036 5.47905 0.288004 5.28705C0.480004 5.09505 0.717337 4.99938 1 5.00005H12.15L9.3 2.15005C9.1 1.95005 9 1.71238 9 1.43705C9 1.16172 9.1 0.924382 9.3 0.725049C9.5 0.525049 9.73767 0.425049 10.013 0.425049C10.2883 0.425049 10.5257 0.525049 10.725 0.725049L15.3 5.30005C15.4 5.40005 15.471 5.50838 15.513 5.62505C15.555 5.74172 15.5757 5.86672 15.575 6.00005C15.575 6.13338 15.554 6.25838 15.512 6.37505C15.47 6.49172 15.3993 6.60005 15.3 6.70005L10.7 11.3C10.5167 11.4834 10.2877 11.575 10.013 11.575C9.73834 11.575 9.50067 11.475 9.3 11.275Z" fill="white"/>
+                </svg>                    
+              </button>
+            </form>
+          </div>
+        </div>
+        <div class="-bottom-1 left-0 absolute">
+            <svg width="631" height="681" viewBox="0 0 631 681" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M630.226 0.795562C541.029 0.795558 452.705 18.3643 370.298 52.4986C287.89 86.6328 213.013 136.664 149.941 199.736C86.8688 262.808 36.8374 337.686 2.70305 420.093C-31.4313 502.501 -49 590.825 -49 680.022L162.818 680.022C162.818 618.641 174.908 557.861 198.397 501.152C221.887 444.444 256.316 392.917 299.719 349.514C343.122 306.111 394.648 271.682 451.357 248.193C508.065 224.703 568.845 212.613 630.226 212.613L630.226 0.795562Z" fill="url(#paint0_linear_1_749)"/>
+                <defs>
+                <linearGradient id="paint0_linear_1_749" x1="33.7263" y1="680.022" x2="504.277" y2="189.719" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" stop-opacity="0.2"/>
+                <stop offset="1" stop-color="white" stop-opacity="0"/>
+                </linearGradient>
+                </defs>
+            </svg>                
+        </div>
+        <div class="-bottom-1 left-40 absolute">
+            <svg width="680" height="681" viewBox="0 0 680 681" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M679.226 0.795562C590.029 0.795558 501.705 18.3643 419.298 52.4986C336.89 86.6328 262.013 136.664 198.941 199.736C135.869 262.808 85.8374 337.686 51.7031 420.093C17.5687 502.501 2.96899e-05 590.825 2.96899e-05 680.022L211.818 680.022C211.818 618.641 223.908 557.861 247.397 501.152C270.887 444.444 305.316 392.917 348.719 349.514C392.122 306.111 443.648 271.682 500.357 248.193C557.065 224.703 617.845 212.613 679.226 212.613L679.226 0.795562Z" fill="url(#paint0_linear_1_750)"/>
+                <defs>
+                <linearGradient id="paint0_linear_1_750" x1="82.7263" y1="680.022" x2="553.277" y2="189.719" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" stop-opacity="0.2"/>
+                <stop offset="1" stop-color="white" stop-opacity="0"/>
+                </linearGradient>
+                </defs>
+            </svg>                
         </div>
     </section>
 
