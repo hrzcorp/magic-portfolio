1217e8d14ec9c5b6f24caa0565f0e9dc3a8edd92
Ahmad Afif Aulia Hariz <ahmadafif.hariz@gmail.com>
2025-03-02 14:24:03 +0700

    fix bug and issues

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -8,12 +8,25 @@ use Illuminate\Support\Facades\Config;
 class TelegramService
 {
     protected $telegramUrl;
+    protected $botToken = null;
+    protected $chatId = null;
 
     public function __construct()
     {
-        $this->telegramUrl = 'https://api.telegram.org/bot' . Config::get('services.telegram.bot_token');
+        if ($this->botToken === null) {
+            $this->botToken = Config::get('services.telegram.bot_token');
+            if ($this->botToken === null) {
+                throw new \Exception('Telegram bot token is not set in the configuration file.');
+            }
+        }
+        if ($this->chatId === null) {
+            $this->chatId = Config::get('services.telegram.chat_id');
+            if ($this->chatId === null) {
+                throw new \Exception('Telegram chat ID is not set in the configuration file.');
+            }
+        }
+        $this->telegramUrl = 'https://api.telegram.org/bot' . $this->botToken;
     }
-
     /**
      * Send a message to the specified chat.
      *
@@ -23,10 +36,10 @@ class TelegramService
     public function sendMessage(string $message): void
     {
         try {
-            $chatId = Config::get('services.telegram.chat_id');
-
+            logger()->debug('Telegram URL: '.$this->telegramUrl); // Log debug for debugging purposes.
+            
             Http::post("{$this->telegramUrl}/sendMessage", [
-                'chat_id' => $chatId,
+                'chat_id' => $this->chatId,
                 'text' => $message,
             ]);
         } catch (\Throwable $th) {
