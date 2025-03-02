17d6547a2ca0ea4b2a98af78ac5cdd41f1435083
hrzcorp-id <hrzcorp.id@gmail.com>
2025-03-02 18:41:16 +0700

    add seo improvement (#20)
    Co-authored-by: Ahmad Afif Aulia Hariz <ahmadafif.hariz@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="HRZ Indonesia provides IT solutions, including software development, cloud services, and automation. Your trusted technology partner.">
     <title>{{ env('APP_NAME') }} | {{ $title ?? '' }}</title>
     @vite('resources/css/app.css')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
